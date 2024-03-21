cc1a66377ad7a88e22226af7780e4a6d769c9342
rubemalmeida <rubem.almeida@stone.com.br>
2024-03-21 17:57:20 -0300

    Adicionando teste para garantir o funcionamento correto da rota de conversão da API

---
+++ tests/test_api.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from app.api import app
+
+client = TestClient(app)
+
+
+def test_convert_currency():
+    response = client.post(
+        "/converter_moeda/",
+        json={"moeda_origem": "USD", "moeda_destino": "BRL", "valor": 100},
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data.get("moeda_origem") == "USD"
+    assert data.get("moeda_destino") == "BRL"
+    assert data.get("valor") == 100
+    assert data.get("valor_convertido") > 0
